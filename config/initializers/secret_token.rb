4f4c5e59d288b2e04a076772b7b4ce3dd670c823
Peter Moran <workingpeter@gmail.com>
2011-11-08 23:11:53 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DelayedPaperclipSample::Application.config.secret_token = '7d4acbe048ab389071a1a7639e3687c0dd6a26bd09dce7a23960df2099280fe6d2953f824d4d21822092f345f91d4421095980441f839a746264803a92041acc'
